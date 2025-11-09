b5c250ee66724503796eb442e9f1b811baf5084e
MDuque11 <mduque@usm.cl>
2025-11-09 17:56:55 -0300

    pantalla de inicio y botones cuestionario

--- teo/config/settings/settings.py
+++ teo/config/settings/settings.py
@@ -61,6 +61,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
